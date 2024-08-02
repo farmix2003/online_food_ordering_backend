bdf46f1b213170bdf93b086e7afcd5ffe6791b03
Farmix2003 <ffarrux386@gmail.com>
2024-08-02 18:23:54 +0300

    Address, Category, ContactInfo, Extras, Food, Order, OrderFood, Restaurant, User, USER_ROLE Entities are completed

--- src/main/java/com/farmix/entity/Category.java
+++ src/main/java/com/farmix/entity/Category.java
@@ -1,9 +1,7 @@
 package com.farmix.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,4 +16,10 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String name;
+
+    @JsonIgnore
+    @ManyToOne
+    private Restaurant restaurant;
 }
